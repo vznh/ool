6dee32b506e1dc391fb9bf121d95825a54e0498b
Jason Son <jasonson2004@gmail.com>
2025-02-06 20:28:48 -0800

    new GET method added to support pull-req func

--- back/src/server.rs
+++ back/src/server.rs
@@ -8,14 +8,17 @@ use axum::{
 // use serde_json::json;
 use std::net::SocketAddr;
 // use reqwest
-use crate::handlers::maintainers::get_recent_commits_handler;
+use crate::handlers::maintainers::{
+  get_freq_of_merged_pull_requests_handler, get_recent_commits_handler,
+};
 
 async fn github_full_repo_refresher() -> impl IntoResponse {}
 
 pub async fn main() {
   let app = Router::new()
     .route("/g", get(github_full_repo_refresher))
-    .route("/m/:repository/:username", get(get_recent_commits_handler));
+    .route("/m/:repository/:username", get(get_recent_commits_handler))
+    .route("/pr/:repository/:owner", get(get_freq_of_merged_pull_requests_handler));
 
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   println!("Successfully listening on {}", addr);
