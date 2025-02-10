b83ae8c1d1cf4e290f4ba70e519ac6e188c0c8eb
Jason Son <jasonson2004@gmail.com>
2025-02-09 17:36:36 -0800

    Main file was edited in favor of aggregated route

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,17 +1,9 @@
 // main.rs
-use axum::{
-  response::IntoResponse,
-  routing::get,
-  Router,
-};
+use axum::{response::IntoResponse, routing::get, Router};
+use back::routes::parse_if_active::pia_handler;
+use reqwest::Client;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use reqwest::Client;
-use back::routes::parse_if_active::pia_handler;
-use back::handlers::maintainers::{
-  get_freq_of_merged_pull_requests_handler, get_recent_commits_handler,
-};
-use back::handlers::toprepos::get_top_repos_handler;
 
 async fn github_full_repo_refresher() -> impl IntoResponse {}
 
@@ -19,16 +11,13 @@ pub async fn serve() {
   let client = Arc::new(Client::new());
   let app = Router::new()
     .route("/g", get(github_full_repo_refresher))
-    .route("/pia/:repository/:owner", get(pia_handler))
-    .route("/m/:repository/:username", get(get_recent_commits_handler))
-    .route("/pr/:repository/:owner", get(get_freq_of_merged_pull_requests_handler))
-    .route("/top-repos", get(get_top_repos_handler));
+    .route("/pia/:repository_name/:owner_username", get(pia_handler))
+  ;
 
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   println!("Successfully listening on {}. You can now make requests.", addr);
   axum_server::bind(addr).serve(app.into_make_service()).await.unwrap();
 }
-
 
 #[tokio::main]
 async fn main() {
